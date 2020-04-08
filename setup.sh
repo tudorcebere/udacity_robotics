8f77d212053d0e2b39f2d63a246f814f13d9fb75
tudorcebere <31571425+tudorcebere@users.noreply.github.com>
2020-04-08 22:04:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/workspace/udacity_robotics/build
 export GAZEBO_MODEL_PATH=/home/workspace/udacity_robotics
 git config --global user.email "tudorcebere@gmail.com"
 git config --global user.name "tudorcebere"
